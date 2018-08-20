eb4cf93c1429af0fab88e245ca96c4bd86b7f470
David S <david@schwarzfr.com>
2018-08-20 23:09:05 +0200

    Added exercises

--- j03Exec/test.c
+++ j03Exec/test.c
@@ -1,46 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dschwarz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/08 18:08:28 by dschwarz          #+#    #+#             */
-/*   Updated: 2018/08/15 15:56:25 by dschwarz         ###   ########.fr       */
+/*   Created: 2018/08/15 16:08:40 by dschwarz          #+#    #+#             */
+/*   Updated: 2018/08/15 16:25:26 by dschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_atoi(char *str)
+int ft_atoi(char *str)
 {
 	int i;
-	int neg;
 	int nb;
+	int neg;
 
 	i = 0;
-	neg = 1;
 	nb = 0;
+	neg = 1;
 	while (str[i] <= 32)
+	{
 		i++;
+	}
 	if ((str[i] == 43 || str[i] == 45) && str[i + 1] >= 48 && str[i + 1] <= 57)
 	{
 		if (str[i] == 45)
 			neg = -1;
-		i++;
+			i++;
 	}
 	while (str[i] >= 48 && str[i] <= 57)
 	{
-		nb = nb * 10 + (str[i] - 48);
+		nb = (nb * 10) + (str[i] - 48);
 		i++;
 	}
 	nb = nb * neg;
-	return (nb);
+	return(nb);
 }
 
 int main()
 {
-	char s[] = "   	-+125fef48";
-	printf("%d", ft_atoi(s));
+	char a[] = "		   +1258eefwe";
+	printf("%d", ft_atoi(a));
 }
-
